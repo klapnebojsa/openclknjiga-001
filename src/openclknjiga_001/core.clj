f3efffb8a6c4461d9ba9068efc51d2e7857a0d0a
klapnebojsa <klapnebojsa@gmail.com>
2016-02-24 02:00:05 +0100

    .

--- src/openclknjiga_001/core.clj
+++ src/openclknjiga_001/core.clj
@@ -14,13 +14,16 @@
              [structs :refer [wrap-byte-seq int8]]])
   (:import [org.jocl CL]))
 (facts
-(let [notifications (chan)
-      follow (register notifications)work-sizes (work-size [1])
-      host-msg (direct-buffer 16)
-      program-source 
-      "__kernel void hello_kernel(__global char16 *msg) {\n    *msg = (char16)('H', 'e', 'l', 'l', 'o', ' ',
+  (let [notifications (chan)
+        follow (register notifications)work-sizes (work-size [1])
+        host-msg (direct-buffer 16)
+        program-source 
+        "__kernel void hello_kernel(__global char16 *msg) {\n    *msg = (char16)('H', 'e', 'l', 'l', 'o', ' ',
    'k', 'e', 'r', 'n', 'e', 'l', '!', '!', '!', '\\0');\n}\n"
+        pr-sor (slurp (io/reader "examples/exam001"))
       ]
+    
+ 
   (try     ;"PLATFORME ----------------------------------------------------------" 
     (with-release [num-platforms (num-platforms)
                    ;"-----Platforma 1"
@@ -37,8 +40,8 @@
                    icd-suffix-khrtwo (icd-suffix-khr platformstwo) 
                    extensionstwo (extensions platformstwo)                   
                    name-infotwo (name-info platformstwo)
-                   profiletwo (profile platformstwo)
-                   vendortwo (vendor platformstwo)                   
+                     profiletwo (profile platformstwo)
+                     vendortwo (vendor platformstwo)                   
 
             ;"DEVICES------------------------------------------------------------"
                    ;"--------Devices u platformi 1"
@@ -59,33 +62,46 @@
                    ;"--------Devices u platformi 2"                   
                    devstwo (first (devices platformstwo))
                    devstwogpu (first (devices platformstwo :gpu))
-                   devstwocpu (first (devices platformstwo :cpu))
-                   devstwoacc (first (devices platformstwo :accelerator))                   
+                     devstwocpu (first (devices platformstwo :cpu))
+                     devstwoacc (first (devices platformstwo :accelerator))                   
                    devicestwo* (devices* platformstwo 8)                   
                    name-infodevtwogpu (name-info devstwogpu)
                    
             ;"CONTEXTS------------------------------------------------------------"  
                   ;"---Context 1"               
-                   ctxone (context (devices (first (platforms))))   ;devsone-izlaz je vektor pa zato ponavaljamo celu komandu a ne uzimamo promenjivu
+                   ;ctxone (context (devices (first (platforms))))   ;devsone-izlaz je vektor pa zato ponavaljamo celu komandu a ne uzimamo promenjivu           
+                   ;ctxone (context devsone)  ;preko prethodih ucitanih devices. Tada context ima dva uredjaja
+                           ;Greska koja nastaje:
+                           ;     Stack dump:
+                           ;     0.	CTHeader.h:1050:91: current parser token ';'
+                     
+                   ;U context je  dodat samo jedan uredjaj tj procesor. U prethodnom redu su dodata dva uredjaja ali nece da radi (puca)
+                   ;nece da radi sa integrisanom grafickom kartom a neintegrisana grafika pripada drugoj platform tako da nemoze da se kombinuje sa procesorom
+                   ;samo neintegrisana grafika nece da se smesti u context - izbacuje gresku
+                   ctxone (context [devsone1])
+                   
                    infoctxone (info ctxone)
                    reference-countone (reference-count ctxone)
                    propertiesone (properties ctxone)
                   ;"--------Devices u context 1"            
                    devices-in-context (devices-in-context ctxone) 
                    num-devices-in-context (num-devices-in-context ctxone)
+                   ;iz konteksta uzimamo uredjaje, a ne iz devices, koji su tu jer smo ih ponovo definisali
                    devctxone1 (first devices-in-context)
-                   devctxone2 (second devices-in-context)
+                   ;devctxone2 (second devices-in-context)
                    name-infodevone1 (name-info devctxone1)
-                   name-infodevone2 (name-info devctxone2)                   
+                   ;name-infodevone2 (name-info devctxone2)                   
                   ;"---Context 2"               
                    ;ctxtwo (context (devices (second (platforms))))
                    ;infoctxtwo (info ctxtwo)
                   ;"--------Devices u context 2"                     
                   
-                  
-                  
-                  
-                  
+             ;"PROGRAM--------------------------------------------------------------"             
+                   cqueue (command-queue-1 ctxone devsone1)
+                   cl-msg (cl-buffer ctxone 16 :write-only)                   
+                   prog (build-program! (program-with-source ctxone [pr-sor]))   
+                   hello-kernel (kernel prog "exam001.core")
+                   read-complete (event)
                   
                    
                    ;devs (devices (first (platforms)))
@@ -97,107 +113,109 @@
                              ;prog (build-program! (program-with-source ctx [program-source]))
                              ;hello-kernel (kernel prog "hello_kernel")
 
-                   ;read-complete (event)
-                   ]
+                     ;read-complete (event)
+                     ]
 
-      (println "----------------------------------------------------------PLATFORME ")
-      (println "num-platforms: " num-platforms) 
-      (println "---------------PLATFORMA 111111111111-------------------------------")      
-      (println "platformsone: " platformsone)
-      (println "versionone: " versionone)
-      (println "icd-suffix-khrone: " icd-suffix-khrone)      
-      (println "extensionsone: " extensionsone)
-      (println "name-infoone: " name-infoone)      
-      (println "profileone: " profileone)      
-      (println "vendorone: " vendorone)
-      (println "")
+        (println "----------------------------------------------------------PLATFORME ")
+        (println "num-platforms: " num-platforms) 
+        (println "---------------PLATFORMA 111111111111-------------------------------")      
+        (println "platformsone: " platformsone)
+        (println "versionone: " versionone)
+        (println "icd-suffix-khrone: " icd-suffix-khrone)      
+        (println "extensionsone: " extensionsone)
+        (println "name-infoone: " name-infoone)      
+        (println "profileone: " profileone)      
+        (println "vendorone: " vendorone)
+        (println "")
       
-      (println "---------------PLATFORMA 222222222222-------------------------------")         
-      (println "platformstwo: " platformstwo) 
-      (println "versiontwo: " versiontwo) 
-      (println "icd-suffix-khrtwo: " icd-suffix-khrtwo)
-      (println "extensionstwo: " extensionstwo)
-      (println "name-infotwo: " name-infotwo)      
-      (println "profiletwo: " profiletwo)      
-      (println "vendortwo: " vendortwo)
-      (println "DOVDE ----------------------------------------------------- PLATFORME")
-      (println "") 
-      (println "")
+        (println "---------------PLATFORMA 222222222222-------------------------------")         
+        (println "platformstwo: " platformstwo) 
+        (println "versiontwo: " versiontwo) 
+        (println "icd-suffix-khrtwo: " icd-suffix-khrtwo)
+        (println "extensionstwo: " extensionstwo)
+        (println "name-infotwo: " name-infotwo)      
+        (println "profiletwo: " profiletwo)      
+        (println "vendortwo: " vendortwo)
+        (println "DOVDE ----------------------------------------------------- PLATFORME")
+        (println "") 
+        (println "")
       
-      (println "------------------------------------------------------------DEVICES")  
-      (println "---------------DEVICE U PLATFORM 1111111111111111 ------------------")
-      (println "devsone: " devsone)
-      (println "devsone1: " devsone1)
-      (println "devsone2: " devsone2)      
-      (println "devsonegpu: " devsonegpu)
-      (println "devsonecpu: " devsonecpu)
-      (println "devsoneacc: " devsoneacc)
-      (println "devicesone*: " devicesone*)
-      (println "-----------Device 1")      
-      (println "name-infodevonegpu: " name-infodevonegpu)
-      (println "opencl-c-ver-devonegpu: " opencl-c-ver-devonegpu)
-      (println "id-infodevonegpu: " id-infodevonegpu) 
-      (println "       ...")
-      (println "-----------Device 2")
-      (println "name-infodevonecpu: " name-infodevonecpu)
-      (println "       ...")
+        (println "------------------------------------------------------------DEVICES")  
+        (println "---------------DEVICE U PLATFORM 1111111111111111 ------------------")
+        (println "devsone: " devsone)
+        (println "devsone1: " devsone1)
+        (println "devsone2: " devsone2)      
+        (println "devsonegpu: " devsonegpu)
+        (println "devsonecpu: " devsonecpu)
+        (println "devsoneacc: " devsoneacc)
+        (println "devicesone*: " devicesone*)
+        (println "-----------Device 1")      
+        (println "name-infodevonegpu: " name-infodevonegpu)
+        (println "opencl-c-ver-devonegpu: " opencl-c-ver-devonegpu)
+        (println "id-infodevonegpu: " id-infodevonegpu) 
+        (println "       ...")
+        (println "-----------Device 2")
+        (println "name-infodevonecpu: " name-infodevonecpu)
+        (println "       ...")
       
-      (println "")
-      (println "---------------DEVICE U PLATFORM 2222222222222222 --------------------")
-      (println "devstwo: " devstwo)
-      (println "devstwogpu: " devstwogpu)
-      (println "devstwocpu: " devstwocpu)
-      (println "devstwoacc: " devstwoacc)
-      (println "devicestwo*: " devicestwo*)
-      (println "name-infodevtwogpu: " name-infodevtwogpu)      
-      (println "DOVDE ----------------------------------------------------- DEVICES")
+        (println "")
+        (println "---------------DEVICE U PLATFORM 2222222222222222 --------------------")
+        (println "devstwo: " devstwo)
+        (println "devstwogpu: " devstwogpu)
+        (println "devstwocpu: " devstwocpu)
+        (println "devstwoacc: " devstwoacc)
+        (println "devicestwo*: " devicestwo*)
+        (println "name-infodevtwogpu: " name-infodevtwogpu)      
+        (println "DOVDE ----------------------------------------------------- DEVICES")
 
-      (println "")
-      (println "------------------------------------------------------------CONTEXT") 
-      (println "ctxone: " ctxone)
-      (println "infoctxone: " infoctxone)
-      (println "reference-countone: " reference-countone)
-      (println "propertiesone: " propertiesone)       
-      (println "---------------DEVICE U CONTEXT 1111111111111111 ------------------")
-      (println "devices-in-context: " devices-in-context)
-      (println "num-devices-in-context: " num-devices-in-context)      
-      (println "devctxone1: " devctxone1)
-      (println "devctxone2: " devctxone2)      
-      (println "name-infodevone1: " name-infodevone1) 
-      (println "name-infodevone2: " name-infodevone2) 
-      
-      (println "---------------DEVICE U CONTEXT 2222222222222222 ------------------")
-      ;(println "ctxtwo: " ctxtwo)
-      ;(println "infoctxtwo: " infoctxtwo)     
-      (println "DOVDE ----------------------------------------------------- CONTEXT")
+        (println "")
+        (println "------------------------------------------------------------CONTEXT") 
+        (println "ctxone: " ctxone)
+        (println "infoctxone: " infoctxone)
+        (println "reference-countone: " reference-countone)
+        (println "propertiesone: " propertiesone)       
+        (println "---------------DEVICE U CONTEXT 1111111111111111 ------------------")
+        (println "devices-in-context: " devices-in-context)
+        (println "num-devices-in-context: " num-devices-in-context)      
+        (println "devctxone1: " devctxone1)
+        ;(println "devctxone2: " devctxone2)      
+        (println "name-infodevone1: " name-infodevone1) 
+        ;(println "name-infodevone2: " name-infodevone2) 
+        
+        (println "---------------DEVICE U CONTEXT 2222222222222222 ------------------")
+        ;(println "ctxtwo: " ctxtwo)
+        ;(println "infoctxtwo: " infoctxtwo)     
+        (println "DOVDE ----------------------------------------------------- CONTEXT")
 
+        
+        ;(println "dev1: " dev)
+        ;(println "ctx1: " ctx)
+        ;(println "cqueue1: " cqueue) 
+        ;(println "prog1: " prog)
+        ;(println "hello-kernel1: " hello-kernel)
+        
+        ;(println "dev1: " info dev)
+        ;(println "ctx1: " info ctx)
+        ;(println "cqueue1: " info cqueue) 
+        ;(println "prog1: " info prog)
+        ;(println "hello-kernel1: " info hello-kernel)
       
-      ;(println "dev1: " dev)
-      ;(println "ctx1: " ctx)
-      ;(println "cqueue1: " cqueue) 
-      ;(println "prog1: " prog)
-      ;(println "hello-kernel1: " hello-kernel)
-      
-      ;(println "dev1: " info dev)
-      ;(println "ctx1: " info ctx)
-      ;(println "cqueue1: " info cqueue) 
-      ;(println "prog1: " info prog)
-      ;(println "hello-kernel1: " info hello-kernel)
-      
-      
-      ;(println "read-complete1: " info read-complete)
+        
+        ;(println "read-complete1: " info read-complete)
 
-      ;(set-args! hello-kernel cl-msg)
-      ;(enq-nd! cqueue  hello-kernel work-sizes)
-      ;(enq-read! cqueue cl-msg host-msg)
-      ;(apply str (map char
-      ;               (wrap-byte-seq int8 (byte-seq host-msg))))
-      (release platforms)
-      (release devices)
-      (release context)      
-      ;(release-seq (platforms)) 
-      ;(Releaseable (platforms))      
-      
+        ;(set-args! hello-kernel cl-msg)
+        ;(enq-nd! cqueue  hello-kernel work-sizes)
+        ;(enq-read! cqueue cl-msg host-msg)
+        ;(apply str (map char
+        ;               (wrap-byte-seq int8 (byte-seq host-msg))))
+        (release platforms)
+        (release devices)
+        (release ctxone)
+        (release prog)
+        
+        ;(release-seq (platforms)) 
+        ;(Releaseable (platforms))      
+        
       )
     (catch Exception e (println "Greska 11111111: " (.getMessage e)))
   )
